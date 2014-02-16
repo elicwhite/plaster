d77e32daeed3663e1c5d7769a76471cdedc7ac42
Eli White <github@eli-white.com>
2014-02-16 16:16:24 +1000

    Enabled gzip compression for node, enabled app cache. PRODUCTION VERSION

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + "/build"));
+app.configure(function() {
+  app.use(express.compress());
+  app.use(express.static(__dirname + "/build"));
+});
+
+
 
 var port = process.env.PORT || 8800;
 app.listen(port);
