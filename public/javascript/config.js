794cb52773846e038a769ff66c12c8efdd42bcf8
Eli White <github@eli-white.com>
2014-03-22 18:15:16 +1100

    Removing IndexedDB Shim

--- public/javascript/config.js
+++ public/javascript/config.js
@@ -1,6 +1,5 @@
 require.config({
   paths: {
-    'indexedDBShim': 'vendor/indexedDBShim',
     'promise': 'vendor/promise-0.1.1',
     'db': 'vendor/db',
     'realtime': 'vendor/realtime-client-utils'
